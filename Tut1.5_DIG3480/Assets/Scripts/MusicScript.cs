03205d920a659a8ebf84ad3899762e50973a33a5
LeeEimer <AinsleyE@knights.ucf.edu>
2023-02-28 11:48:24 -0500

    Added Changing Animation Code

--- Tut1.5_DIG3480/Assets/Scripts/MusicScript.cs
+++ Tut1.5_DIG3480/Assets/Scripts/MusicScript.cs
@@ -9,11 +9,13 @@ public class MusicScript : MonoBehaviour
     public AudioClip musicClipOne;
 
     public AudioClip musicClipTwo; 
+
+     Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,17 +24,21 @@ public class MusicScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.W)){
             musicSource.clip = musicClipOne;
             musicSource.Play(); 
+            anim.SetInteger("State", 1);
         }
         if(Input.GetKeyUp(KeyCode.W)){
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
         //audio clip 2
         if(Input.GetKeyDown(KeyCode.R)){
             musicSource.clip = musicClipTwo;
             musicSource.Play(); 
+            anim.SetInteger("State", 2);
         }
         if(Input.GetKeyUp(KeyCode.R)){
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
         //add loop
          if (Input.GetKeyDown(KeyCode.L)){
